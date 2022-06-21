3e49f2fb778621065fb7727b58496a4e82a1ac8f
SUNNY KUMAR <95949944+SunnyK9325@users.noreply.github.com>
2022-06-21 23:30:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+//random image generator for player 1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+
+//random image generator for player 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if(randomNumber1 > randomNumber2)
+{
+  document.querySelector("h1").textContent = "🚩Player 1 wins !";
+}
+else if(randomNumber1 < randomNumber2)
+{
+  document.querySelector("h1").textContent = "Player 2 wins ! 🚩";
+}
+else
+document.querySelector("h1").textContent = "Draw!";
